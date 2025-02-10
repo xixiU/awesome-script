a31cfecf7bf30e8f22fb963fe6abf5a2cffc37c2
rongjie.yuan <rongjie.yuan@jiduauto.com>
2025-02-10 16:50:56 +0800

    sql generate add some example

---
+++ SQLGeneratorHelper/output/result_content2.sql
@@ -0,0 +1,3 @@
+INSERT INTO  result_content2(反馈内容, result_第一次, result_第二次, result, gmt_create) values('在实际的行驶过程当中处于在车道的中间但是在我们的屏幕的渲染上面，车道车辆在车道之外。用户会担心车辆后续再开启智驾当中，也会因这样的问题导致智驾的一个风险。' ,'True' ,'True' ,'1.0' ,) ;
+INSERT INTO  result_content2(反馈内容, result_第一次, result_第二次, result, gmt_create) values('就是在试驾的时候，这个试驾路线在城区，没有城区自驾那个ASD用不了 客户说我们这个宣传是有带虚假的，说城区全部可以开通了，但是时间还是用不了' ,'True' ,'True' ,'1.0' ,) ;
+INSERT INTO  result_content2(反馈内容, result_第一次, result_第二次, result, gmt_create) values('前方车辆停止状态，ASD无法识别进行自动避让或者变道' ,'True' ,'True' ,null,) ;
