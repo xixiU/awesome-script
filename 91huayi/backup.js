27bb2193c3e0d4abb1861a00f9b8413d0fb7913f
rongjie.yuan <rongjie.yuan@jiduauto.com>
2025-07-12 10:09:20 +0800

    add backup js

--- 91huayi/backup.js
+++ 91huayi/backup.js
@@ -1,8 +1,11 @@
 // 课程列表页
-const listItems = document.querySelectorAll('.itemList .contentItem .type .ready');
-let nextVideoItem = 0;
-for (const item of listItems) {
-    if (item.textContent.trim() !== '已观看') {
-        nextVideoItem = nextVideoItem + 1;
+function listNeedLLearn() {
+    const listItems = document.querySelectorAll('.itemList .contentItem .type .ready');
+    let nextVideoItem = 0;
+    for (const item of listItems) {
+        if (item.textContent.trim() !== '已观看') {
+            nextVideoItem = nextVideoItem + 1;
+        }
     }
+    return nextVideoItem
 }
