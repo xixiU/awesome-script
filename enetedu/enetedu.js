be94b949cd8155947832ecba85d00bace0e5336f
rongjie.yuan <rongjie.yuan@jiduauto.com>
2025-08-04 23:20:47 +0800

    check 5s actual 5/2 seconds

--- enetedu/enetedu.js
+++ enetedu/enetedu.js
@@ -217,7 +217,7 @@
                 this.handleVideoComplete();
             } else {
                 // 每3秒打印一次视频进度日志
-                if (currentTime - this.lastLogTime >= 3) {
+                if (currentTime - this.lastLogTime >= 5) {
                     utils.log(`当前视频进度: ${currentTime}s/${duration}s，播放速度: ${video.playbackRate}倍`);
                     this.lastLogTime = currentTime;
                     this.checkCurrentProgress();
